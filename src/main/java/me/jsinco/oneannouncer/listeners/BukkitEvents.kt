83e27d194530faa1a1f986079dfc96c3bb39f360
Jsinco <jonahmarksinclair@gmail.com>
2023-10-04 00:09:13 -0400

    Broadcasts relay

---
+++ src/main/java/me/jsinco/oneannouncer/listeners/BukkitEvents.kt
@@ -0,0 +1,13 @@
+package me.jsinco.oneannouncer.listeners
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.server.BroadcastMessageEvent
+
+class BukkitEvents : Listener {
+
+    @EventHandler
+    fun onBroadCast(event: BroadcastMessageEvent) {
+
+    }
+}
